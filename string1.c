1d8ae5fc1378380c099ec1f9a2def16f8a2a06ff
Ashok <68994450+AshokDon@users.noreply.github.com>
2024-11-28 18:33:34 +0530

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,40 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 #include<stdlib.h>
+//string handling functions
+
 int main(void) {
-    //how to read as an input
-    //string functions
-    //1.strlen --> length of the string
-    char C[] ="jfhajsdflh";
-    size_t n = strlen(C);
-    //strcpy
-    char scr[6] = "hello";
-    char dest[10];
-    //strncpy
-    strncpy(dest,scr,2);
-    printf("%s",dest);
     
-    //strcat
-    char scr1[6] = "hello";
-    char scr2[6] = "hello";
-    strcat(scr1,scr2);
-    printf("%s",scr1);
+    //1.strlen() function used to find the length of the given string
+    //example
+    char str[] = "Hello, world";
+    size_t len = strlen(str);
     
-    //strncat
-    strncat(scr1,scr2,3);
-    printf("%s",scr1);
+    //2.strcpy() function used to copy the  entire string to the other
+    //example
+    char src[] = "Hello";
+    char dest[20]; // dest size should be greater then or equal to the src size
+    strcpy(dest,src);
+    //printf("%s",dest);
     
-    //strcmp
-    int res = strcmp(scr1,scr2);
-    //if res == 0 then they are equal other wise not equal
-    //strncmp
-    char A[10] ="abc";
-    char B[10] = "abcded";
-    int res1 = strncmp(scr1,scr2,3);
+    //3.strncpy() function used to copy exactly n character from src to dest
+    strncpy(dest,src,3);
+    dest[3] ='\0';
     
-    //strstr
-    char s[20] = "hello world";
-    char* pos = strstr(s,"world");
+    //4.strcat() function used to concatenate two strings
+    char str1[20] = "Hello"; // should need to mention the size for the str1
+    //because we are adding str2 to str1
+    char str2[] = " Wolrd";
+    strcat(str1,str2);
+    printf("%s",str1);
+    
+    //5.strncat() function used to concatenate n characters from str2
+    strncat(str1,str2,3);
+    
+    //6.strcmp() function used to compare whether two strings are equal or not
+    //if equal returns 0 otherwise return any other nonzero number
+    char s1[] = "Hello";
+    char s2[] = "Hello";
+    int res = strcmp(s1,s2);
+    
+    //7.strchr() function used to find the position of a given character in a string
+    char s3[] = "Hello, World";
+    char *pos = strchr(s3,'W');
+    //if(pos)printf("%ld",pos-s3);
+    
+    //8.strstr( function used to find the give string present in the original
+    //string
+    char s4[] = "Hello, World";
+    char *p = strstr(s4,"World");//returns the address of the first character in
+    //the World
+    //if(p) printf("Substring found:%s\n",p);
+    
+    
     
     
     
